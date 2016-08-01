f34ffc53a4daf5b06973fb74f127585ca5eb40b7
Hat Dao <enjoy3013@gmail.com>
2016-08-01 18:13:46 +0800

    Add worker `python3 bot.py trackemon` to scrawl from trackemon with pokemon list config

--- bot.py
+++ bot.py
@@ -7,15 +7,19 @@ import asyncio
 import discord
 import json
 import logging
+import os.path
 import re
+import requests
 import sys
+import time
 
+from enums import Pokemon
 from urllib.request import Request, urlopen
 
 client = discord.Client()
 
 # config logging
-logging.basicConfig(filename = 'messages.log', level = logging.WARNING)
+logging.basicConfig(filename='messages.log', level=logging.WARNING)
 
 config = {}
 with open('config.json') as output:
@@ -26,22 +30,86 @@ api_key = config.get('api_key', 'NA')
 channels = config.get('channels', [])
 except_roles = config.get('except_roles', [])
 
-"""
-Check if there is blacklisted word in message or not
-"""
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
 def is_blacklisted(message_content):
+    """ Check if there is blacklisted word in message or not """
+
     blacklist = config.get('blacklist', [])
 
-    # If blacklist is empty dont use black list 
+    # If blacklist is empty dont use black list
     if len(blacklist) == 0:
         return False
 
     for word in blacklist:
         if word in message_content:
-            return True    
+            return True
+
+
+def scrawl_trackemon(pokemon_name):
+    """ Scrawl api from trackemon.com """
+
+    api_root = 'http://www.trackemon.com/'
+    pokemon_name = pokemon_name.upper()
+
+    # get pokemon id
+    try:
+        pokemon_id = getattr(Pokemon, pokemon_name).value
+
+    except AttributeError:
+        print('Wrong pokemon name')
+        sys.exit()
+
+    api_endpoint = '{}fetch/rare?pokedexTypeId={}&sessionId={}'
+
+    # start sessions to keep session/cookies
+    session = requests.session()
+    # get session_id by fetching root
+    first_response = session.get('http://www.trackemon.com/')
+
+    pattern = re.compile(r'sessionId\s=\s\'\w+\'')
+    session_id_string = re.search(
+        pattern, first_response.content.decode('utf-8'))
+    if session_id_string:
+        # Found string, split session id
+        session_id = session_id_string.group().replace(
+            'sessionId = ', '').replace('\'', '')
+    else:
+        raise Exception('Can\'t retrieve session id for api.')
+
+    endpoint = api_endpoint.format(api_root, pokemon_id, session_id)
+    print(endpoint)
+    response = session.get(endpoint)
+
+    data = response.json()
+
+    message = ''
+    for pokemon in data:
+        expire_seconds = int(pokemon['expirationTime']) - time.time()
+        # format message
+        message += '`{}` {},{} - Expires in: {:.0f} seconds \n'.format(
+            _get_pokemon_name(pokemon['pokedexTypeId']),
+            pokemon['latitude'],
+            pokemon['longitude'],
+            expire_seconds
+        )
+
+    return message
+
+
+def _get_pokemon_name(pokemon_id):
+    pokemon_id = int(pokemon_id)
+    for pokemon in Pokemon:
+        if pokemon.value == pokemon_id:
+            return pokemon.name.lower().replace('_', '').capitalize()
+
+    return 'NA'
+
 
 if api_key == 'NA':
     raise Exception('Please specify your api key!')
+
 
 @client.event
 async def on_ready():
@@ -51,14 +119,14 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-"""
-python3 bot.py scrawl
-"""
+#
+# python3 bot.py scrawl
+#
 if len(sys.argv) > 1 and sys.argv[1] == 'scrawl':
-    """
-    Worker to get rare pokemons from http://pokesnipers.com/api/v1/pokemon.json
-    """
+
     async def get_rare_pokemons():
+        """ Worker to get rare pokemons from http://pokesnipers.com/api/v1/pokemon.json """
+
         await client.wait_until_ready()
 
         # get arrays channels id need to post
@@ -70,15 +138,14 @@ if len(sys.argv) > 1 and sys.argv[1] == 'scrawl':
 
         if len(shout_out_channels) == 0:
             raise Exception("No channel to shout out!")
-            
 
         while not client.is_closed:
 
-            print('Scrawling..')
+            print('Scrawling PokeSnipers..')
 
             req = Request(
                 'http://pokesnipers.com/api/v1/pokemon.json',
-                headers = {
+                headers={
                     'User-Agent': 'Mozilla/5.0',
                     'Cache-Control': 'max-age=0, private, must-revalidate'
                 }
@@ -86,24 +153,61 @@ if len(sys.argv) > 1 and sys.argv[1] == 'scrawl':
             json_string = urlopen(req).read()
             data = json.loads(json_string.decode('utf-8'))
 
-            message =  ''
+            message = ''
             for pokemon in data.get('results'):
-                message += "`{}` {}\n".format(pokemon.get('name'), pokemon.get('coords'))
+                message += "`{}` {}\n".format(pokemon.get('name'),
+                                              pokemon.get('coords'))
 
             for channel in shout_out_channels:
                 await client.send_message(channel, message)
-            
+
             await asyncio.sleep(config.get('delay_scrawl', 300))
 
     # Loop task
     client.loop.create_task(get_rare_pokemons())
 
+elif len(sys.argv) > 1 and sys.argv[1] == 'trackemon':
+    #
+    # crawl trackemon
+    #
+    if config.get('scrawl_trackemon') and config.get('scrawl_trackemon')['enabled']:
+        async def scrawl_trackemon_worker():
+
+            await client.wait_until_ready()
+
+            # get arrays channels id need to post
+            shout_out_channels = []
+            for server in client.servers:
+                for channel in server.channels:
+                    if channel.name in config.get('scrawl_channels', []):
+                        shout_out_channels.append(discord.Object(channel.id))
+
+            if len(shout_out_channels) == 0:
+                raise Exception("No channel to shout out!")
+
+            while not client.is_closed:
+                print('Scrawling Trackemon..')
+
+                if 'pokemons' in config.get('scrawl_trackemon'):
+                    pokemon_names = config.get('scrawl_trackemon')['pokemons']
+                    for pokemon_name in pokemon_names:
+                        message = scrawl_trackemon(pokemon_name)
+                        
+                        if len(message):
+                            for channel in shout_out_channels:
+                                await client.send_message(channel, message)
+
+                await asyncio.sleep(config.get('delay_scrawl', 300) * len(pokemon_names))  # increase delay to finish task
+
+        # Loop task
+        client.loop.create_task(scrawl_trackemon_worker())
+
 else:
-    """
-    Event loop on every message receive
-    """
     @client.event
     async def on_message(message):
+        """
+        Event loop on every message receive
+        """
         # Do nothing if bot is sending message
         if message.author == client.user:
             return None
@@ -124,18 +228,19 @@ else:
 
         # Manage channel rare_spottings only
         if message.channel.name in channels:
-            if not (re.match(pattern, message.content) or re.match(reverse_pattern, message.content)) or is_blacklisted(message.content):      
+            if not (re.match(pattern, message.content) or re.match(reverse_pattern, message.content)) or is_blacklisted(message.content):
                 # Log & print out
-                log_message = 'Message has been deleted: {} - Author: {}'.format(message.content, message.author.name)
+                log_message = 'Message has been deleted: {} - Author: {}'.format(
+                    message.content, message.author.name)
                 print(log_message)
                 logging.warning(log_message)
 
                 # Delete message if not contain lat/long
                 await client.delete_message(message)
 
-    """
-    Announcement
-    """
+    #
+    # Announcement
+    #
     print(config.get('announcement'))
     if config.get('announcement', None):
         async def announcement():
@@ -156,12 +261,13 @@ else:
             for server in client.servers:
                 for channel in server.channels:
                     if channel.name in channels:
-                        announcement_channels.append(discord.Object(channel.id))
+                        announcement_channels.append(
+                            discord.Object(channel.id))
 
             while not client.is_closed:
                 for channel in announcement_channels:
                     await client.send_message(channel, message)
-            
+
                 await asyncio.sleep(delay)
 
         # Loop task
